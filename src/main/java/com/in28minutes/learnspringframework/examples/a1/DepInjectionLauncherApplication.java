fa0679fec28d7781b6ba6d3e0381ef01ca87f93e
ameeraazli <wanazliwanameera@gmail.com>
2023-06-16 11:58:43 +0800

    The sample usage of *Setter-based* dependency injection

--- src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -11,10 +11,20 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 @Component
 class YourBusinessClass{
-    @Autowired
     Dependency1 dependency;
-    @Autowired
     Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency(Dependency1 dependency) {
+        System.out.println("Setter Injection - setDependency");
+        this.dependency = dependency;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection - setDependency2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString() {
         return "Using " + dependency + " and " + dependency2;
