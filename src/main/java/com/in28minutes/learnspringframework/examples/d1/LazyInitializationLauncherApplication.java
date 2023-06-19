4b6db29a850d9dd123dc717462e002925c60a924
ameeraazli <wanazliwanameera@gmail.com>
2023-06-19 15:39:59 +0800

    Sample implementation of Eager Initialization

--- src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
@@ -3,8 +3,24 @@ package com.in28minutes.learnspringframework.examples.d1;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+
+@Component
+class ClassA {
+
+}
+
+@Component
+class ClassB {
+    private ClassA classA;
+    public ClassB(ClassA classA){
+        //Logic
+        System.out.println("Some Initialization logic");
+        this.classA = classA;
+    }
+}
 
 @Configuration
 @ComponentScan
@@ -14,8 +30,7 @@ public class LazyInitializationLauncherApplication {
         try (var context =
                      new AnnotationConfigApplicationContext
                              (LazyInitializationLauncherApplication.class)) {
-            Arrays.stream(context.getBeanDefinitionNames())
-                    .forEach(System.out::println);
+          
         }
     }
 }
